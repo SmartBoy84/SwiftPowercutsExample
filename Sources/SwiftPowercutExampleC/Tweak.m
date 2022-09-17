a083afa247153c21d74bf05786874c79f23799d5
_Qubarf <barfie@gabba.ga>
2022-09-17 22:35:00 +0800

    Update Tweak.m

--- Sources/SwiftPowercutExampleC/Tweak.m
+++ Sources/SwiftPowercutExampleC/Tweak.m
@@ -1,17 +1,7 @@
 #import <Orion/Orion.h>
 #import <libpowercuts/libpowercuts.h>
 
-// fun fact! theos does -emit-objc-header automatically on compilation (exists
-// in the final module dir)
-#import "SwiftPowercutExample-Swift.h"
-
 __attribute__((constructor)) static void init() {
   // Initialize Orion - do not remove this line.
   orion_init();
-
-  // register action - put all registrations here
-  [[PowercutsManager sharedInstance]
-      registerActionWithIdentifier:
-          @"com.barfie.automaton.action.complexAction"
-                            action:[ComplexAction new]];
 }
